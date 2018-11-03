fe7370fb768669f09fbd61ba5a17bd1cfd890617
jamailun <jamailun@laposte.net>
2018-11-03 13:02:41 +0100

    Updated GameWatcher.java
    Added the timed out server sized system.

--- src/fr/jamailun/pong/server/GameWatcher.java
+++ src/fr/jamailun/pong/server/GameWatcher.java
@@ -13,11 +13,30 @@ public class GameWatcher implements Runnable {
 	@Override
 	public void run() {
 		try {
+			check = 50;
 			while(PongServerMain.state != State.STOP) {
 				
 				Thread.sleep(20);
 				
 				mgr.refreshBall();
+				
+				check--;
+				final long last1 = PongServerMain.client1.getLastKeepAlive();
+				final long last2 = PongServerMain.client2.getLastKeepAlive();
+				final long now = System.currentTimeMillis();
+				if(check <= 0) {
+					//après 10 sec : timeout !
+					if(now - last1 >= 10000) {
+						System.out.println("[SERVER][WARN] Client 1 has timed out !");
+						PongServerMain.disconnect(PongServerMain.client1);
+					}
+					if(now - last2 >= 10000) {
+						System.out.println("[SERVER][WARN] Client 2 has timed out !");
+						PongServerMain.disconnect(PongServerMain.client2);
+					}
+					check = 0;
+				}
+				
 				
 			}
 		} catch (InterruptedException e) {
